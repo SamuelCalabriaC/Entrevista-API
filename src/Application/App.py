f0c9889ea6cb476e440cbdd4b0f815cde291f636
samuel <s.calabriac@gmail.com>
2022-03-18 16:35:24 +0100

    Python API - HP

--- src/Application/App.py
+++ src/Application/App.py
@@ -1,4 +1,7 @@
 from src.Services.ApiService import ApiService
+from sys import stderr
+import pandas as pd
+from datetime import datetime
 
 
 class App:
@@ -7,3 +10,31 @@ class App:
 
     def api_service(self) -> ApiService:
         return self._api_service
+
+    def process_to_csv(self):
+        return_val = self.api_service().run()
+        if isinstance(return_val, str):
+            return False
+        else:
+            return Processing(return_val)
+
+
+class Processing:
+    def __init__(self, alldata):
+        try:
+            print('- Iterate over all items, cast to DF and then to csv.')
+            for iter_dict in alldata:
+                # print(iter_dict)
+                panda_cast = pd.DataFrame.from_dict(iter_dict, orient='index').T
+                # print(panda_cast)
+                act_date = datetime.today().strftime('%Y_%m_%d')+'_'+str(iter_dict.get('id'))
+                panda_cast.to_csv('../../storage/' + str(act_date) + '.csv', index_label=False)
+            print('- All .csv created')
+        except Exception as e:
+            print(e)
+            print('- An error occurred while converting to .csv ', file=stderr)
+
+
+if __name__ == '__main__':
+    app = App()
+    data = app.process_to_csv()
--- src/Services/ApiService.py
+++ src/Services/ApiService.py
@@ -1,11 +1,24 @@
 from sys import stderr
+import requests
 
 
 class ApiService:
+    endpoint = 'https://jsonplaceholder.typicode.com/todos/'
+
     def __init__(self):
         pass
 
+    # Calls get method.
     def run(self):
         print('Running ApiService', file=stderr)
+        return self.__get()
 
-        # TODO: follow README.md instructions
+    # GET request to endpoint
+    def __get(self):
+        print('- Sending Get Request to the Endpoint')
+        r = requests.get(self.endpoint)
+        if r.status_code == 200:
+            print('- GET Request Status Code: ' + str(r.status_code))
+            return r.json()
+        else:
+            return 'There is something wrong.'
